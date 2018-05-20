b57b4229b939b4ea53ece210254dce9453ea5318
Souji 2613 <puppypoola@gmail.com>
2018-05-20 22:53:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -42,7 +42,8 @@ function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
   return array;
 }
-function removeElementFromEndOfArray(array{
-  
+function removeElementFromEndOfArray(array){
+  array.slice(0,array - 1);
+  return array ;
   
 }
